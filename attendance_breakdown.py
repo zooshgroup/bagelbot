7aeb025bd8b75d8d26f7b84733ba2e05bfca1902
Ben Hayden <hayden767@gmail.com>
2016-06-21 17:10:21 -0500

    Add attendance breakdown script.

---
+++ attendance_breakdown.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Simple script for generating some simple meeting attendance statistics
+using history of past meetings.
+"""
+from __future__ import print_function
+
+from pprint import pprint
+
+from utils import open_store
+
+store = open_store()
+everyone = store['everyone']
+history = store['history']
+store.close()
+
+attendance = {p: {'total': 0, 'dates': []} for p in everyone}
+for meeting in history:
+    for pair in meeting['attendees']:
+        for person in pair:
+            if person in attendance:
+                attendance[person]['total'] += 1
+                attendance[person]['dates'].append(meeting['date'])
+
+for person, info in attendance.iteritems():
+    print('=== {} ==='.format(person))
+    print(' Total Attended: {}'.format(info['total']))
+    print(' Meeting Dates: {}'.format(', '.join(d.strftime('%m/%d/%Y') for d in sorted(info['dates']))))
+    print()
